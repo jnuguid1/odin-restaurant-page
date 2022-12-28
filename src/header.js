c612b5b1f927653dc961776c5e3728eeed1bf38f
Jared <jarednuguid@gmail.com>
2022-12-28 15:04:21 -0500

    Change header and footer into modules

---
+++ src/header.js
@@ -0,0 +1,59 @@
+import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
+
+const loadHeader = () => {
+  const content = document.querySelector('#content');
+  const header = document.createElement('div');
+  header.classList.add('header');
+  const headerTitle = document.createElement('h1');
+  headerTitle.textContent = "Lola Gloria's Kusina";
+  
+  const nav = document.createElement('ul');
+  const homeTab = document.createElement('li');
+  const menuTab = document.createElement('li');
+  const contactTab = document.createElement('li');
+  const homeTabText = document.createElement('a');
+  const menuTabText = document.createElement('a');
+  const contactTabText = document.createElement('a');
+  homeTabText.textContent = 'Home';
+  menuTabText.textContent = 'Menu';
+  contactTabText.textContent = 'Contact';
+  homeTab.appendChild(homeTabText );
+  menuTab.appendChild(menuTabText);
+  contactTab.appendChild(contactTabText);
+  homeTab.addEventListener('click', switchTabHome);
+  menuTab.addEventListener('click', switchTabMenu);
+  contactTab.addEventListener('click', switchTabContact);
+  
+  nav.appendChild(homeTab);
+  nav.appendChild(menuTab);
+  nav.appendChild(contactTab);
+
+  header.appendChild(headerTitle);
+  header.appendChild(nav);
+  content.appendChild(header);
+}
+
+const switchTabHome = () => {
+  const content = document.querySelector('#content');
+  var currentTab = content.childNodes[1];
+  content.removeChild(currentTab);
+  loadHome();
+}
+
+const switchTabMenu = () => {
+  const content = document.querySelector('#content');
+  var currentTab = content.childNodes[1];
+  content.removeChild(currentTab);
+  loadMenu();
+}
+
+const switchTabContact = () => {
+  const content = document.querySelector('#content');
+  var currentTab = content.childNodes[1];
+  content.removeChild(currentTab);
+  loadContact();
+}
+
+export default loadHeader;
